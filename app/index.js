3c064e0d462a7eae18d59b52a855abf9992eab48
justin leggett <justin@codingrookie.com>
2017-04-21 15:13:15 -0500

    added var justin to test out the this keyword

--- app/index.js
+++ app/index.js
@@ -6,6 +6,16 @@ import Props1 from './components/Props1';
 
 var family = ["Justin", "Katie", "Kallie", "Zoey", "Nox", "Pax"];
 
+var justin = {
+    name: "justin leggett",
+    birthday: "09/30/1981",
+    sayName: function() {
+        console.log("name = " + this.name);
+    }
+}
+
+justin.sayName();
+
 class App extends React.Component {
     render() {
     return (
